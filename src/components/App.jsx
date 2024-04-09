b958b3b1aebd032782e2e8cbe601b5c64ecff413
Vita <v.mars22@ukr.net>
2024-04-09 21:57:43 +0300

    add description

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@
 
 // eslint-disable-next-line no-unused-vars
 import React, { useEffect, useState } from 'react';
-import Description from "./description/Description.jsx";
+import Description from './description/Description.jsx';
 import Option from './options/Options.jsx';
 import Feedback from './feedback/Feedback.jsx';
 import Notification from './notification/notification.jsx';
